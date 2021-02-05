9b61e6b0a3e80ff06fa0bb56939b375c202904f4
HelyeFab <emmanuelfabiani23@gmail.com>
2021-02-05 12:23:07 +0000

    submitting prototype for app

--- js/fetchRequests.js
+++ js/fetchRequests.js
@@ -1,22 +1,40 @@
-import { formRecipe} from './main.js'
-
+import { formRecipe } from './main.js';
 
 const userInput = document.querySelector('.userInput');
 const drinkSelection = document.querySelector('.drinkSelection');
 const inputForm = document.querySelector('.inputForm');
+const errorMessage = document.querySelector('.errorMessage');
 
 export function fetchCocktail() {
   let choice = userInput.value;
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`;
+  console.log(choice);
+  if (choice === '') {
+    errorMessage.classList.add('active');
+    return;
+  } else {
+    errorMessage.classList.remove('active');
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`;
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       const { drinks } = data;
-      displayAvailableDrinks(drinks);
+      if(drinks === null){
+        errorMessage.classList.add('active');
+      } else {
+        errorMessage.classList.remove('active');
+        displayAvailableDrinks(drinks);
+
+      }
+      
+     
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
+
+  }
+
+  
 }
 // Displaying all the drinks matching the user Input in the UI
 
@@ -30,9 +48,12 @@ export function displayAvailableDrinks(drinks) {
     let label = document.createElement('label');
     label.for = drink.strDrink;
     label.textContent = drink.strDrink;
+    let br = document.createElement('br');
+    
     inputForm.appendChild(input);
     // inputArray.push(input)
     inputForm.appendChild(label);
+    inputForm.appendChild(br);
   });
 }
 
@@ -40,16 +61,16 @@ export function fetchDrinksRecipe(input) {
   const optionsArray = inputForm.childNodes;
   optionsArray.forEach((el) => {
     if (el.checked === true) {
-    let drinkSelected = el.attributes[1].textContent;
+      let drinkSelected = el.attributes[1].textContent;
       const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkSelected}`;
       fetch(url)
         .then((res) => res.json()) // parse response as JSON
         .then((data) => {
-          const { drinks } = data
+          const { drinks } = data;
           let matchedDrinkObj = drinks.filter(
             (drink) => drink.strDrink === drinkSelected
           );
-          formRecipe(matchedDrinkObj)
+          formRecipe(matchedDrinkObj);
         })
         .catch((err) => {
           console.log(`error ${err}`);
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import {
   fetchCocktail,
-  displayAvailableDrinks,
   fetchDrinksRecipe,
 } from './fetchRequests.js';
 
@@ -19,7 +18,7 @@ getRecipeBtn.addEventListener('click', fetchDrinksRecipe);
 export function formRecipe(drink) {
   //! Creating an object for the selected Drink
 
-  // Cleaning the object from the empty key value pairs
+  //Step 1: Cleaning the object from the empty key value pairs
   function clean(obj) {
     for (let propName in obj) {
       if (obj[propName] === null || obj[propName] === undefined) {
@@ -28,43 +27,58 @@ export function formRecipe(drink) {
     }
     return obj;
   }
-  // Obtaining the object correspondent to the user choice
+  //Step 2: Obtaining the object correspondent to the user choice
   const userDrink = clean(drink[0]);
 
-  // Obtaining the ingredient for the chosen drink
+  //Step 3: Obtaining the ingredient for the chosen drink
   const ingredients = Object.entries(userDrink).reduce(
     (acc, [key, val]) => (key.includes('strIngredient') ? [...acc, val] : acc),
     []
   );
-  // Obtaining the measures for the chosen drink
+  //Step 4: Obtaining the measures for the chosen drink
   const measures = Object.entries(userDrink).reduce(
     (acc, [key, val]) => (key.includes('strMeasure') ? [...acc, val] : acc),
     []
   );
 
- 
-
   //? Changing the Recipe elements according to the selected drink
-  // Making Visible the recipe section
+  //Step 1: Making Visible the recipe section
   const recipeCard = document.querySelector('.recipeCard');
   recipeCard.classList.add('active');
-  // Recipe's name
+  //Step 2: Showing the Recipe's name
   document.querySelector('.recipeTitle').textContent = userDrink.strDrink;
-  // Showing a picture of the Drink
-  document.querySelector('.imgContainer').firstElementChild.src = `${userDrink.strDrinkThumb}`
+  //Step 3: Showing a picture of the Drink
+  document.querySelector(
+    '.imgContainer'
+  ).firstElementChild.src = `${userDrink.strDrinkThumb}`;
 
+  //Step 4: Combining Ingredients and measures into a single array
+  let ingredientsMeasuresArray = [];
+  for (var i = 0; i < ingredients.length; i++) {
+    if (measures[i] != undefined) {
+      ingredientsMeasuresArray.push(`${ingredients[i]}: ${measures[i]}`);
+    } else {
+      ingredientsMeasuresArray.push(`${ingredients[i]}`);
+    }
+  }
 
-  // Creating and appending the list of ingredient in the UI
+ 
+// Step: 5 Creating and appending the list of ingredient in the UI
+
   let textNode;
 
-  ingredients.forEach((ingredient) => {
+  ingredientsMeasuresArray.forEach((el) => {
     let node = document.createElement('li');
-    textNode = document.createTextNode(ingredient);
+    textNode = document.createTextNode(el);
     node.appendChild(textNode);
     document.querySelector('.recipeIngredients').appendChild(node);
     // Inserting the recipe in the UI
     document.querySelector('.cocktailRecipe').textContent =
-    userDrink.strInstructions;
+      userDrink.strInstructions;
   });
+
+
+
+
  
 }
