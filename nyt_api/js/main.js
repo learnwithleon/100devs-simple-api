a5a8ef1ec57a7b59eef8fc9448fd3a19408478fa
kent auyeung <kentayeung@gmail.com>
2021-02-05 18:13:11 -0800

    Made changes to NYT API (wip)

--- nyt_api/js/main.js
+++ nyt_api/js/main.js
@@ -4,41 +4,33 @@ document.querySelector('button').addEventListener('click', fetchHeadlines)
 
 function fetchHeadlines() {
 const choice = document.querySelector('.choice').value
-// const articles = data.response.docs
 const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${choice}&api-key=mhD3OliI6H1iB98ReEZvOd0jGVn0CYzG`
+
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      console.log(data)
-      console.log(data.response.docs[0])
-      printHeadlines(data.response.docs)
+      // console.log(data)
+      // article 1
+      document.querySelector('.url1').innerText = data.response.docs[0].abstract
+      document.querySelector('.url1').href = data.response.docs[0].web_url
+      document.querySelector('.author1').innerText = data.response.docs[0].byline.original
+      document.querySelector('.pubdate1').innerText = data.response.docs[0].pub_date
+      document.querySelector('.line1').innerText = "-------------------------------------------------------------"
+      // article 2
+      document.querySelector('.url2').innerText = data.response.docs[1].abstract
+      document.querySelector('.url2').href = data.response.docs[1].web_url
+      document.querySelector('.author2').innerText = data.response.docs[1].byline.original
+      document.querySelector('.pubdate2').innerText = data.response.docs[1].pub_date
+      document.querySelector('.line2').innerText = "-------------------------------------------------------------"
+      //article 3
+      document.querySelector('.url3').innerText = data.response.docs[2].abstract
+      document.querySelector('.url3').href = data.response.docs[2].web_url
+      document.querySelector('.author3').innerText = data.response.docs[2].byline.original
+      document.querySelector('.pubdate3').innerText = data.response.docs[2].pub_date
+      // document.querySelector('.line3').innerText = "-------------------------------------------------------------"
+      // document.querySelector('.url1').innerText = data.response.docs[0].web_url
     })
     .catch(err => {
       console.log(`error ${err}`)
     })
 }
-
-// function createDiv() {
-//   let div = document.createElement('div')
-//   div.innerText
-// }
-
-function printHeadlines(arr) {
-  let h3 = document.createElement('h3')
-  for(i=0; i<arr.length; i++) {
-    // console.log(getAbstract(arr[i]))
-    // document.querySelector('h3').innerText = getHeadlines(arr[i])
-    document.body.appendChild(h3).innerText = getHeadlines(arr[i])
-  }
-}
-
-
-function getHeadlines(obj) {
-  for(let key in obj) {
-    console.log(obj.abstract)
-    return obj.abstract
-  }
-}
-
-
-// headline, link, author
