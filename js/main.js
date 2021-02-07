407296752cccae0d5cb5cccad8c53bd2feab5cb5
Nicole Lee <nicolees1123@gmail.com>
2021-02-06 20:33:39 -0800

    add api files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  // const choice = document.querySelector('input').value
+  const url = 'https://api.quotable.io/random'
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('h2').innerText = data.content
+        document.querySelector('h3').innerText = data.author
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
