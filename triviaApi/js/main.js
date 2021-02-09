69ed53fff7f69e54be8021a0a9f7f602fb67a7bd
Din Ho <dinvho@gmail.com>
2021-02-08 16:14:18 -0800

    added Giphy api calls to the Trivia api project

--- triviaApi/js/main.js
+++ triviaApi/js/main.js
@@ -14,7 +14,7 @@ function question(){
       document.querySelector('h1').innerText = newQuestion
       document.querySelector('#answer').addEventListener('click', answer =>{
         document.querySelector('p').innerText = correctAnswer
-        getGif()
+        getGif(correctAnswer)
       })
     })
     .catch(err => {
@@ -22,17 +22,18 @@ function question(){
     })
 }
 
-let apiKey = 'v7JJFLu9ajLhAWZf2wDCl2mjOFwijONU'
-let url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`
-function getGif(){
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        let random = Math.floor(Math.random()*51)
-        let gif = data.data[random].images.downsized.url
-        document.querySelector('img').src = gif
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+function getGif(triviaAnswer){
+  let name = triviaAnswer
+  let apiKey = 'v7JJFLu9ajLhAWZf2wDCl2mjOFwijONU'
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=${name}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    let gif = data.data[0].images.downsized.url
+    document.querySelector('img').src = gif
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
 }
